7cef365a006136b7b681ca324c34547f68ca5677
RudraMThakkar <thakkarrudram@gmail.com>
2026-07-19 21:11:48 +0530

    error fix

--- config.py
+++ config.py
@@ -1,13 +1,11 @@
 import os
-class config:
-    SECRET_KEY = 
-"ce_chatbot_secret_key"
+class Config:
+    SECRET_KEY = "ce_chatbot_secret_key"
 
     #WhatsApp Cloud API
     ACCESS_TOKEN = ""
     PHONE_NUMBER_ID = ""
-    VERIFY_TOKEN =
-"ce_chatbot_verify_token"
+    VERIFY_TOKEN ="ce_chatbot_verify_token"
 
     #Database
     DATABASE = "chatbot.db"
