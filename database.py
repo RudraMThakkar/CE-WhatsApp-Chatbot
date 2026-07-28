3822d86598ef77ae91449d0591de0a5be79b0bf8
RudraMThakkar <thakkarrudram@gmail.com>
2026-07-28 11:37:12 +0530

    database.py redesign

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 """
 database.py
 
-Creates and manages the chatbot database.
+Database for CE WhatsApp Chatbot
 """
 
 import sqlite3
@@ -10,19 +10,31 @@ DATABASE_NAME = "chatbot.db"
 
 
 def initialize_database():
-    """
-    Creates the chatbot database and required tables.
-    """
 
     conn = sqlite3.connect(DATABASE_NAME)
     cursor = conn.cursor()
 
-    # Store user conversation state
+    # User State Table
     cursor.execute("""
-        CREATE TABLE IF NOT EXISTS user_state (
-            user_id TEXT PRIMARY KEY,
-            state TEXT NOT NULL
-        )
+    CREATE TABLE IF NOT EXISTS user_state(
+        user_id TEXT PRIMARY KEY,
+        state TEXT NOT NULL
+    )
+    """)
+
+    # Chatbot Knowledge Table
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS chatbot_data(
+
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+        category TEXT NOT NULL,
+
+        subcategory TEXT NOT NULL,
+
+        response TEXT NOT NULL
+
+    )
     """)
 
     conn.commit()
@@ -30,16 +42,13 @@ def initialize_database():
 
 
 def save_state(user_id, state):
-    """
-    Save or update a user's current state.
-    """
 
     conn = sqlite3.connect(DATABASE_NAME)
     cursor = conn.cursor()
 
     cursor.execute("""
-        INSERT OR REPLACE INTO user_state(user_id, state)
-        VALUES (?, ?)
+    INSERT OR REPLACE INTO user_state(user_id,state)
+    VALUES(?,?)
     """, (user_id, state))
 
     conn.commit()
@@ -47,16 +56,14 @@ def save_state(user_id, state):
 
 
 def get_state(user_id):
-    """
-    Returns the current state of the user.
-    """
 
     conn = sqlite3.connect(DATABASE_NAME)
     cursor = conn.cursor()
 
     cursor.execute("""
-        SELECT state FROM user_state
-        WHERE user_id=?
+    SELECT state
+    FROM user_state
+    WHERE user_id=?
     """, (user_id,))
 
     result = cursor.fetchone()
@@ -67,3 +74,38 @@ def get_state(user_id):
         return result[0]
 
     return "MAIN_MENU"
+
+
+def insert_data(category, subcategory, response):
+
+    conn = sqlite3.connect(DATABASE_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    INSERT INTO chatbot_data(category,subcategory,response)
+    VALUES(?,?,?)
+    """, (category, subcategory, response))
+
+    conn.commit()
+    conn.close()
+
+
+def get_data(category, subcategory):
+
+    conn = sqlite3.connect(DATABASE_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    SELECT response
+    FROM chatbot_data
+    WHERE category=? AND subcategory=?
+    """, (category, subcategory))
+
+    result = cursor.fetchone()
+
+    conn.close()
+
+    if result:
+        return result[0]
+
+    return "Information not available."
